b81082c711e8e3ea291351f01e575850b00e38a0
Subham sahu <sahusubham38632@gmail.com>
2026-09-22 15:13:37 +0530

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def relu(x) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    # Write code here
+    x = np.array(x)
+    return np.array(np.maximum(0,x))
